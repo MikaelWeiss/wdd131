7e10e98f2c1fb9293323a60c353573ead534ebf6
Mikael Weiss <campingmikael@icloud.com>
2025-06-14 19:47:30 -0600

    updated to use template

--- register/register.js
+++ register/register.js
@@ -1,46 +1,6 @@
-let participantsCount = 1;
+import { participantTemplate, successTemplate } from './Templates.js';
 
-function participantTemplate(count) {
-  return `
-    <section class="participant${count}">
-            <p>Participant ${count}</p>
-            <div class="item">
-              <label for="fname${count}"> First Name<span>*</span></label>
-              <input id="fname${count}" type="text" name="fname" value="" required />
-            </div>
-            <div class="item activities">
-              <label for="activity${count}">Activity #<span>*</span></label>
-              <input id="activity${count}" type="text" name="activity" />
-            </div>
-            <div class="item">
-              <label for="fee${count}">Fee ($)<span>*</span></label>
-              <input id="fee${count}" type="number" name="fee" />
-            </div>
-            <div class="item">
-              <label for="date${count}">Desired Date <span>*</span></label>
-              <input id="date${count}" type="date" name="date" />
-            </div>
-            <div class="item">
-              <p>Grade</p>
-              <select id="grade${count}">
-                <option selected value="" disabled selected></option>
-                <option value="1">1st</option>
-                <option value="2">2nd</option>
-                <option value="3">3rd</option>
-                <option value="4">4th</option>
-                <option value="5">5th</option>
-                <option value="6">6th</option>
-                <option value="7">7th</option>
-                <option value="8">8th</option>
-                <option value="9">9th</option>
-                <option value="10">10th</option>
-                <option value="11">11th</option>
-                <option value="12">12th</option>
-              </select>
-            </div>
-          </section>
-  `;
-}
+let participantsCount = 1;
 
 function addParticipant() {
   participantsCount++;
@@ -66,13 +26,6 @@ function submitForm(event) {
   const summaryElement = document.getElementById('summary');
   summaryElement.innerHTML = successMessage;
   summaryElement.classList.remove('hide');
-}
-
-function successTemplate(info) {
-  const { adultName, totalFeesValue, participantsCount } = info;
-  return `
-    <p>Thank you ${adultName} for registering. You have registered ${participantsCount} participants and owe $${totalFeesValue} in Fees.</p>
-  `;
 }
 
 function totalFees() {
